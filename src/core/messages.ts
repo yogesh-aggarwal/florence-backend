82275557bd52fc5d56fa0a70eca22fc8c0697cd1
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-08-14 10:37:31 +0530

    Added more response messages

--- src/core/messages.ts
+++ src/core/messages.ts
@@ -1,5 +1,7 @@
 export enum ResponseMessages {
-	AUTH_INVALID_REQUEST = "Invalid request",
-	AUTH_INVALID_BODY_CONTENT = "Invalid body contents",
+	SUCCESS = "Success",
+	INVALID_REQUEST = "Invalid request",
+	INVALID_BODY_CONTENT = "Invalid body contents",
+
 	AUTH_PASSWORD_INVALID = "Password is invalid",
 }
