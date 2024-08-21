ab9497015d906d6d8acdfe42c16b7f1cfb6ca12e
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-08-21 16:48:39 +0530

    Fixed `/product/getByIDS` route

--- src/api/v1/routers/product/getByIDS.ts
+++ src/api/v1/routers/product/getByIDS.ts
@@ -1,17 +1,30 @@
 import { Request, Response } from "express"
 import { Document } from "mongoose"
+import { z } from "zod"
+import { parseRequestBody } from "../../core/helpers"
 import { ResponseMessages } from "../../core/messages"
 import { ProductModel } from "../../models/product"
 import { Product_t } from "../../models/product.types"
 
 // --------------------------------------------------------------------------------------
 
+const bodySchema = z.object({
+	ids: z.array(z.string()),
+})
+
+// --------------------------------------------------------------------------------------
+
 export default async function getProductsByIDS(req: Request, res: Response) {
-	const ids: string[] = req.body["ids"]
+	const body = parseRequestBody<z.infer<typeof bodySchema>>(req, bodySchema)
+	if (!body) {
+		return res
+			.status(400)
+			.send({ message: ResponseMessages.INVALID_BODY_CONTENT })
+	}
 
 	// TODO: View counting here? Do we need to increase the view count of the product?
 	const products: Document<Product_t>[] = await ProductModel.find({
-		_id: { $in: ids },
+		_id: { $in: body.ids },
 	})
 	const parsedProducts = products.map((product) => product.toObject())
 
