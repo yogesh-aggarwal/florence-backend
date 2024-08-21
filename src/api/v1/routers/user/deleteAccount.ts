92e39944ec64a6a6bb605421ba8d95e699575916
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-08-21 16:54:39 +0530

    Fixed `/user/deleteAccount` endpoint

--- src/api/v1/routers/user/deleteAccount.ts
+++ src/api/v1/routers/user/deleteAccount.ts
@@ -1,43 +1,26 @@
 import { Request, Response } from "express"
-import jwt from "jsonwebtoken"
-import { Document } from "mongoose"
 
-import { JWT_SECRET } from "../../../../core/constants"
+import { getRequestingUser } from "../../core/helpers"
 import { ResponseMessages } from "../../core/messages"
 import { UserModel } from "../../models/user"
 
 // --------------------------------------------------------------------------------------
 
 export default async function deleteUserAccount(req: Request, res: Response) {
-	// Retrieving the data
-	const { email } = req.body
-
-	// If the provided email is not available in the database, return error
-	const user: Document | null = await UserModel.findOne({
-		email: { $eq: email },
-	})
+	const user = getRequestingUser(req)
 	if (!user) {
-		res.status(401).send({ message: ResponseMessages.INVALID_REQUEST })
-		return
+		return res.status(401).send({ message: ResponseMessages.INVALID_REQUEST })
 	}
 
-	// Generate a token and verify it with the existing token
-	const token = req.headers["authorization"]?.replace("Bearer ", "")
-	if (!token) {
-		res.status(401).send({ message: ResponseMessages.INVALID_REQUEST })
-		return
-	}
-	const tokenemail: string = jwt.verify(token, JWT_SECRET) as string
-
-	// If the token user name (extracted above) is not equal to the provided email, return error
-	if (email != tokenemail) {
-		res.status(401).send({ message: ResponseMessages.INVALID_REQUEST })
-		return
+	const doesExists = await UserModel.exists({ email: user.email })
+	if (!doesExists) {
+		return res.status(401).send({ message: ResponseMessages.INVALID_REQUEST })
 	}
 
-	// Delete user's information from the database and return a success message
-	await UserModel.deleteOne({ email: { $eq: email } })
-	res.status(200).send({ message: ResponseMessages.SUCCESS })
+	// TODO: Delete user's other information as well (like orders, etc.)
+	await UserModel.deleteOne({ email: user.email })
+
+	return res.status(200).send({ message: ResponseMessages.SUCCESS })
 }
 
 // --------------------------------------------------------------------------------------
