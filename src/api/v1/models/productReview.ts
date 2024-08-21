de5f623507fc21903fb41f977853cedcab521c2c
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-08-21 11:42:16 +0530

    Improved schema validation of product review with zod

--- src/api/v1/models/productReview.ts
+++ src/api/v1/models/productReview.ts
@@ -1,41 +1,29 @@
-import mongoose from "mongoose"
+import mongoose, { model, Schema } from "mongoose"
 import { ProductReview_t } from "./productReview.types"
 
-const productReviewSchema = new mongoose.Schema({
-	id: {
-		type: String,
-		required: true,
-		trim: true,
-	},
-	userID: {
-		type: String,
-		required: true,
-		trim: true,
-	},
-	starsGiven: {
-		type: Number,
-		required: true,
-		min: 0,
-	},
-	review: {
-		type: String,
-		required: true,
-		trim: true,
-		min: 0,
-	},
-	whenReviewed: {
-		type: Date,
-		required: true,
-		trim: true,
-	},
-	userName: {
-		type: String,
-		required: true,
-		trim: true,
-	},
+// --------------------------------------------------------------------------------------
+
+const ProductReviewMetadataSchema = new Schema({
+	createdBy: { type: String, required: true },
+	createdAt: { type: Number, required: true },
+	editedAt: { type: Number, required: true },
+	isEdited: { type: Boolean, required: true },
 })
 
-export const ProductReview = mongoose.model<ProductReview_t>(
-	"productReviews",
-	productReviewSchema
+const ProductReviewSchema = new Schema({
+	_id: { type: mongoose.Types.ObjectId, required: true },
+	metadata: { type: ProductReviewMetadataSchema, required: true },
+	productID: { type: String, required: true },
+	starsGiven: { type: Number, required: true },
+	reviewContent: { type: String, required: true },
+})
+
+// --------------------------------------------------------------------------------------
+
+export const ProductReview = model<ProductReview_t>(
+	"review",
+	ProductReviewSchema,
+	"reviews"
 )
+
+// --------------------------------------------------------------------------------------
--- src/api/v1/models/productReview.types.ts
+++ src/api/v1/models/productReview.types.ts
@@ -1,8 +1,27 @@
-export type ProductReview_t = {
-	id: string
-	userID: string
-	starsGiven: number
-	review: string
-	whenReviewed: Date
-	userName: string
-}
+import { ObjectId } from "mongodb"
+import { z } from "zod"
+
+// --------------------------------------------------------------------------------------
+
+export const ProductReviewMetadata_t = z.object({
+	createdBy: z.string(),
+	createdAt: z.number(),
+
+	editedAt: z.number(),
+	isEdited: z.boolean(),
+})
+export type ProductReviewMetadata_t = z.infer<typeof ProductReviewMetadata_t>
+
+// --------------------------------------------------------------------------------------
+
+export const ProductReview_t = z.object({
+	_id: z.instanceof(ObjectId),
+	metadata: ProductReviewMetadata_t,
+
+	productID: z.string(),
+	starsGiven: z.number(),
+	reviewContent: z.string(),
+})
+export type ProductReview_t = z.infer<typeof ProductReview_t>
+
+// --------------------------------------------------------------------------------------
