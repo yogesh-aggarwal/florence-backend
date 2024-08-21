28bbd0e8fe13f4a3eb209f958e6b9299fb7e4930
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-08-21 16:47:25 +0530

    Fixed `/platform/home` route

--- src/api/v1/routers/platform/home.ts
+++ src/api/v1/routers/platform/home.ts
@@ -1,10 +1,33 @@
 import { Request, Response } from "express"
+import { Document } from "mongoose"
+import { getRequestingUser } from "../../core/helpers"
 import { ResponseMessages } from "../../core/messages"
 import { PlatformModel } from "../../models/platform"
 import { PlatformHomeSection_t } from "../../models/platform.types"
 import { ProductModel } from "../../models/product"
 import { Product_t } from "../../models/product.types"
-import { UserModel } from "../../models/user"
+
+// --------------------------------------------------------------------------------------
+
+async function fetchRecommendedProductIDS(
+	userEmail: string
+): Promise<string[] | null> {
+	const url = "http://127.0.0.1:5000/recommended_products"
+
+	const fetchRes = await fetch(url, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ email: userEmail }),
+	})
+	if (fetchRes.status !== 200) return null
+
+	const fetchResBody = await fetchRes.json()
+	const productIDS = fetchResBody["data"]
+
+	return productIDS
+}
 
 // --------------------------------------------------------------------------------------
 
@@ -19,16 +42,17 @@ export default async function getPlatformHomeData(req: Request, res: Response) {
 				.send({ error: ResponseMessages.INTERNAL_SERVER_ERROR })
 		}
 
-		let response: Record<string, Record<string, Product_t[]>> = {}
+		const response: Record<string, Record<string, Product_t[]> | Product_t[]> =
+			{}
 		for (const { name, productIds } of Object.values(data.sections)) {
-			let categories: Record<string, Product_t[]> = {}
-			for (let category of productIds) {
+			const categories: Record<string, Product_t[]> = {}
+			for (const category of productIds) {
 				const ids = Object.values(category)[0]
 				const categoryName = Object.keys(category)[0]
 
 				const products = await ProductModel.find({ id: { $in: ids } })
-				let arr = []
-				for (let product of products) {
+				const arr = []
+				for (const product of products) {
 					arr.push(product)
 				}
 
@@ -38,42 +62,35 @@ export default async function getPlatformHomeData(req: Request, res: Response) {
 			response[name] = categories
 		}
 
-		let email = req.body["email"]
-		if (!email) return res.status(200).send({ response })
+		const user = getRequestingUser(req)
+		if (!user) {
+			return res
+				.status(404)
+				.send({ error: ResponseMessages.AUTH_USER_DOES_NOT_EXIST })
+		}
 
-		const user = await UserModel.findOne({ email: email })
-
-		const url = "http://127.0.0.1:5000/recommended_products"
-		const method = "POST"
-		const body = { email: email }
-
-		let fetchRes = await fetch(url, {
-			method,
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify(body),
-		})
-
-		if (fetchRes.status != 200) {
+		const productIDS = await fetchRecommendedProductIDS(user.email)
+		if (!productIDS) {
 			return res
 				.status(500)
 				.send({ error: ResponseMessages.INTERNAL_SERVER_ERROR })
 		}
 
-		let ans = await fetchRes.json()
-		let recommended_product_ids = ans["data"]
-		const recommended_products: Product_t[] = await ProductModel.find({
-			id: { $in: recommended_product_ids },
+		const products: Document<Product_t>[] = await ProductModel.find({
+			id: { $in: productIDS },
 		})
-		// response["recommendedProducts"] = recommended_products
-		// response["user"] = user
-		return res.status(200).send({ response })
+		const parsedProducts = products.map((product) => product.toObject())
+		response["recommendedProducts"] = parsedProducts
+
+		return res
+			.status(200)
+			.send({ message: ResponseMessages.SUCCESS, data: response })
 	} catch (error) {
 		console.error("Error in getHomeData:", error)
-		return res.status(500).send({ error: "Internal server error" })
+		return res
+			.status(500)
+			.send({ error: ResponseMessages.INTERNAL_SERVER_ERROR })
 	}
 }
 
 // --------------------------------------------------------------------------------------
-
