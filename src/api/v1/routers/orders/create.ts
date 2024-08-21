e5f9c1db47e8e6ff8f3e7b02504246e289855427
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-08-21 16:40:26 +0530

    Fixed `/orders/create` endpoint

--- src/api/v1/routers/orders/create.ts
+++ src/api/v1/routers/orders/create.ts
@@ -1,24 +1,42 @@
 import { Request, Response } from "express"
 import Razorpay from "razorpay"
+import { z } from "zod"
 import { RAZORPAY_KEY_ID, RAZORPAY_SECRET } from "../../../../core/constants"
+import { generateNanoidOnlyAlphabets } from "../../../../core/utils"
+import { parseRequestBody } from "../../core/helpers"
+import { ResponseMessages } from "../../core/messages"
+
+// --------------------------------------------------------------------------------------
+
+const bodySchema = z.object({
+	amount: z.number().positive("Amount must be positive"),
+})
 
 // --------------------------------------------------------------------------------------
 
 export default async function createOrder(req: Request, res: Response) {
-	var instance = new Razorpay({
+	const body = parseRequestBody<z.infer<typeof bodySchema>>(req, bodySchema)
+	if (!body) {
+		return res
+			.status(400)
+			.send({ message: ResponseMessages.INVALID_BODY_CONTENT })
+	}
+
+	const instance = new Razorpay({
 		key_id: RAZORPAY_KEY_ID,
 		key_secret: RAZORPAY_SECRET,
 	})
 
+	const receiptID = `receipt#${generateNanoidOnlyAlphabets(8)}`
 	const order = await instance.orders.create({
-		amount: +req.body["amount"],
 		currency: "INR",
-		receipt: "receipt#1",
+		receipt: receiptID,
+		amount: body.amount,
 	})
 
-	res.status(200).send({
-		message: "order created successfully",
-		order: order,
+	return res.status(200).send({
+		message: ResponseMessages.SUCCESS,
+		data: order,
 	})
 }
 
--- src/core/utils.ts
+++ src/core/utils.ts
@@ -3,3 +3,7 @@ import { customAlphabet } from "nanoid"
 export function generateNanoid(length: number = 10): string {
 	return customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", length)()
 }
+
+export function generateNanoidOnlyAlphabets(length: number = 10): string {
+	return customAlphabet("abcdefghijklmnopqrstuvwxyz", length)()
+}
