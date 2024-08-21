222952d5988b7e0204ce19fc90a36cd653670ffa
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-08-21 15:02:56 +0530

    Improved `/product/getByIDS` endpoint

--- src/api/v1/routers/product/getByIDS.ts
+++ src/api/v1/routers/product/getByIDS.ts
@@ -1,23 +1,19 @@
 import { Request, Response } from "express"
+import { Document } from "mongoose"
+import { ResponseMessages } from "../../core/messages"
 import { ProductModel } from "../../models/product"
 import { Product_t } from "../../models/product.types"
 
 export default async function getProductsByIDS(req: Request, res: Response) {
 	const ids: string[] = req.body["ids"]
 
-	const products: Product_t[] = await ProductModel.find({ id: { $in: ids } })
-	let reqProducts: any[] = []
-	for (let product of products) {
-		let pro: any = {}
-		pro["title"] = product.title
-		pro["image"] = product.images[0]
-		pro["id"] = product._id.toString()
-
-		reqProducts.push(pro)
-	}
-
-	res.status(200).send({
-		message: "Products fetched",
-		data: reqProducts,
+	// TODO: View counting here? Do we need to increase the view count of the product?
+	const products: Document<Product_t>[] = await ProductModel.find({
+		id: { $in: ids },
 	})
+	const parsedProducts = products.map((product) => product.toObject())
+
+	return res
+		.status(200)
+		.send({ message: ResponseMessages.SUCCESS, data: parsedProducts })
 }
