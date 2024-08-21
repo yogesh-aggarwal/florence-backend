cb3a70d85bf6884810e8d9faf3cf9067c9b7c1fc
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-08-21 15:04:45 +0530

    Improved `/product/trending`

--- src/api/v1/routers/product/trending.ts
+++ src/api/v1/routers/product/trending.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express"
+import { ResponseMessages } from "../../core/messages"
 import { ProductModel } from "../../models/product"
-import { Product_t } from "../../models/product.types"
 
 export default async function getTrendingProducts(req: Request, res: Response) {
-	const products: Product_t[] = await ProductModel.find({}).limit(30)
-	res.status(200).send({
-		message: "Products fetched",
-		data: products,
-	})
+	const products = await ProductModel.find({}).limit(30)
+	const parsedProducts = products.map((product) => product.toObject())
+
+	return res
+		.status(200)
+		.send({ message: ResponseMessages.SUCCESS, data: parsedProducts })
 }
